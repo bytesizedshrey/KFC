5912e70a07e285d253602cc925096e6ab680a5a1
shreyash <shreyiosdev@gmail.com>
2022-08-11 09:48:21 +0530

    Initial Commit

---
+++ KFC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KFC
+//
+//  Created by Shreyash Gajbhiye  on 11/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
